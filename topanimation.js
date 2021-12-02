05d22c1c61529b332b89b4a4633d38c1346a7197
ayumi0023 <77398996+ayumi0023@users.noreply.github.com>
2021-12-02 19:13:20 +0900

    Create topanimation.js

---
+++ topanimation.js
@@ -0,0 +1,8 @@
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},3000); //3秒後にロゴ含め真っ白背景をフェードアウト！
+});
